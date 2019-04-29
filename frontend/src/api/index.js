7b93a34ac8fba6817b9907d22852b87a3535055e
suika1 <vanyoktank1@mail.ru>
2019-04-29 22:48:49 +0300

    make post api request

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -15,3 +15,24 @@ export const get = async ({
   return await response.json();
 };
 
+export const post = async ({
+  url,
+  body = {},
+  options = {},
+  headers = {},
+}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    ...options,
+  });
+
+  return await response.json();
+};
+
+
