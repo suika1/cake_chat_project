5fcfd0cb2bd2d7c5d91d0e17093a9316ab7ce839
suika1 <vanyoktank1@mail.ru>
2019-04-29 22:50:24 +0300

    add messageFormSagas

--- frontend/src/rootSaga.js
+++ frontend/src/rootSaga.js
@@ -3,8 +3,10 @@ import { all } from 'redux-saga/effects';
 
 import chatListSagas from 'modules/ChatList/sagas';
 import chatSagas from 'modules/Chat/sagas';
+import messageFormSagas from 'modules/MessageForm/sagas';
 
 export default  [
   ...chatListSagas,
   ...chatSagas,
+  ...messageFormSagas,
 ];
