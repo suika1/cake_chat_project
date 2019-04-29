2650fc815a4b7ced3187fc3c7bcec8ef69e19c70
suika1 <vanyoktank1@mail.ru>
2019-04-29 22:51:18 +0300

    refactor message component

--- frontend/src/modules/Message/index.js
+++ frontend/src/modules/Message/index.js
@@ -2,49 +2,38 @@ import React from 'react';
 import { Typography, ListItem } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Cookies from 'js-cookie';
+import moment from 'moment';
 
 import styles from './styles.scss';
 
 class Message extends React.PureComponent {
   render() {
-    if (this.props.name === myName) {
-      return (
-        <ListItem className={classes.myMessage}>
-          <div className={classes.messageMain}>
-            <Typography className={classes.text} variant="body2">
-              {text}
-            </Typography>
-            <Typography className={classes.date} variant="caption">
-              {hrs}:{mins}
-            </Typography>
-          </div>
-          <div className={classes.messageHeader}>
-            <Typography className={classes.name} variant="body1">
-              {name}
-            </Typography>
-          </div>
-        </ListItem>
-      );
-    } else {
-      return (
-        <ListItem className={classes.message}>
-          <div className={classes.messageHeader}>
-            <Typography className={classes.name} variant="body1">
-              {name}
-            </Typography>
-          </div>
-          <div className={classes.messageMain}>
-            <Typography className={classes.text} variant="body2">
-              {text}
-            </Typography>
-            <Typography className={classes.date} variant="caption">
-              {hrs}:{mins}
-            </Typography>
-          </div>
-        </ListItem>
-      );
-    }
+    const { data } = this.props;
+    const {
+      sendTime,
+      author,
+      text,
+    } = data;
+
+    const msgTime = moment(sendTime).format('HH:mm');
+
+    return (
+      <ListItem className={styles.myMessage}>
+        <div className={styles.messageMain}>
+          <Typography className={styles.text} variant="body2">
+            {text}
+          </Typography>
+          <Typography className={styles.date} variant="caption">
+            {msgTime}
+          </Typography>
+        </div>
+        <div className={styles.messageHeader}>
+          <Typography className={styles.name} variant="body1">
+            {author}
+          </Typography>
+        </div>
+      </ListItem>
+    );
   }
 }
 
@@ -52,7 +41,7 @@ Message.propTypes = {
   name: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   date: PropTypes.number.isRequired,
-  classes: PropTypes.shape({}),
+  styles: PropTypes.shape({}),
 };
 
 export default withStyles(styles)(Message);
