469e09e61810edcc551675f5d59feca250aa6c2f
suika1 <vanyoktank1@mail.ru>
2019-05-26 19:19:14 +0300

    add check jwt token middleware

---
+++ backend/src/middlewares/check-token/index.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import tokenSecret from '../../config/token-secret';
+import * as utils from '../../utils/utils';
+
+const checkToken = (req, res, next) => {
+  let token = req.headers['x-access-token'] || req.headers.authorization;
+  if (token && token.length && token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
+  }
+
+  if (token) {
+    jwt.verify(token, tokenSecret, (err, decoded) => {
+      if (err) {
+        return utils.generateResponse({
+          res,
+          status: 401,
+          ok: false,
+          error: 'Token is not valid',
+        });
+      }
+
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    return utils.generateResponse({
+      res,
+      status: 400,
+      ok: false,
+      error: 'Auth token is not supplied',
+    });
+  }
+};
+
+export default checkToken;
