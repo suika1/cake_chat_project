fe49e04266b73b2c8698f38409f6976a37478949
suika1 <vanyoktank1@mail.ru>
2019-06-03 22:24:44 +0300

    add authForm sagas and reducer to root

--- frontend/src/rootSaga.js
+++ frontend/src/rootSaga.js
@@ -4,8 +4,10 @@ import { all } from 'redux-saga/effects';
 import chatListSagas from 'modules/chatList/sagas';
 import chatSagas from 'modules/chat/sagas';
 import messageFormSagas from 'modules/messageForm/sagas';
+import authFormSagas from 'modules/auth-form/sagas';
 
 export default  [
+  ...authFormSagas,
   ...chatListSagas,
   ...chatSagas,
   ...messageFormSagas,
