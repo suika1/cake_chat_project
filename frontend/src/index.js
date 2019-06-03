5616b0f3f9637c9b515f2790ebdbf8f77944229b
suika1 <vanyoktank1@mail.ru>
2019-06-03 21:28:47 +0300

    provide history

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './modules/app';
 
+import history from './api/browserHistory';
+
 import 'styles/index.css';
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <Provider store={store}>
       <App />
     </Provider>
