ee500bc95499c8fc22d0c58ee137044ddb526df0
krasotkin <vanyoktank1@gmail.com>
2019-06-19 16:03:02 +0300

    remove setupMongoConnection

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -1,5 +1,3 @@
-import mongoose from 'mongoose';
-
 export const generateResponse = ({
   res,
   status = 200,
@@ -18,11 +16,3 @@ export const generateResponse = ({
     }),
   );
 };
-
-export const setupMongoConnection = () => {
-  const dev_db_url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASS}@cluster0-jgkmz.mongodb.net/cake_chat_db?retryWrites=true`;
-  const mongoDB = process.env.MONGODB_URI || dev_db_url;
-  mongoose.connect(mongoDB, {useNewUrlParser: true});
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-}
