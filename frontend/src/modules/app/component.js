944c43e9825186fb95cf8d8af37a7b7665e6ed3b
eastkorzh <mkorzhev@hotmail.com>
2019-07-05 19:03:21 +0300

    initial auth-form||chats redirect added

--- frontend/src/modules/app/component.js
+++ frontend/src/modules/app/component.js
@@ -1,40 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import routes from 'routes';
 import NotFoundPageRoute from 'routes/not-found-page';
+import { getAuthToken } from 'api/localStorage';
+import * as urls from 'appConfig/appUrls';
 
 import styles from './styles.scss';
 
 export default class App extends React.Component {
-  componentDidMount() {
-    if (!navigator.cookieEnabled) {
-      alert('Включите cookie для комфортной работы с этим сайтом');
-    }
-  }
+	componentDidMount() {
+		if (!navigator.cookieEnabled) {
+			alert('Включите cookie для комфортной работы с этим сайтом');
+		}
+	}
 
-  render() {
-    return (
-      <Router>
-        <div className={styles.app}>
-          <Switch>
-            {routes.map(Route => Route())}
+	render() {
+		return (
+			<Router>
+				<div className={styles.app}>
+					<Switch>
+						{routes.map(Route => Route())}
 
-            <NotFoundPageRoute />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+						<Route exact path='/' render={
+							() => {
+								if (!getAuthToken() || getAuthToken() === '0') {
+									return <Redirect to={{pathname: urls.authForm}} />
+								} else if (getAuthToken() && getAuthToken() !== '0') {
+									return <Redirect to={{pathname: urls.chats}} />
+								}
+							}
+						} />	
+
+						<NotFoundPageRoute />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 App.propTypes = {
-  isFetching: PropTypes.bool,
-  error: PropTypes.string,
+	isFetching: PropTypes.bool,
+	error: PropTypes.string,
 };
 
 App.defaultProps = {
-  isFetching: false,
-  error: '',
+	isFetching: false,
+	error: '',
 }
