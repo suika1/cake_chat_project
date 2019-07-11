6057d674eb7111219c7ca8759dc0d89e9b6e9f1a
suika1 <vanyoktank1@gmail.com>
2019-07-11 20:22:21 +0300

    add DELETE method to cors

--- backend/src/middlewares/cors/index.js
+++ backend/src/middlewares/cors/index.js
@@ -2,7 +2,7 @@ const corsMiddleware = (req, res, next) => {
   res.set('Content-Type', 'application/json')
     // cors headers
     .set('Access-Control-Allow-Origin', '*')
-    .set('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,')
+    .set('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE,')
     .set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 };
