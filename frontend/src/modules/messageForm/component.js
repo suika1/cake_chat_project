9921d4413b00fd7db8cc88b2502733d337e0a925
eastkorzh <mkorzhev@hotmail.com>
2019-07-16 05:00:30 +0300

    improve messageField

--- frontend/src/modules/messageForm/component.js
+++ frontend/src/modules/messageForm/component.js
@@ -5,12 +5,15 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 import { getName } from 'api/localStorage';
+import { ThemeProvider } from '@material-ui/styles';
 
 export default class MessageForm extends React.PureComponent {
   state = {
     textValue: '',
     nameValue: '',
   };
+
+  textField = React.createRef();
 
   //For controlling inputs
   onChange = fieldName => e => this.setState({ [fieldName]: e.target.value });
@@ -25,6 +28,8 @@ export default class MessageForm extends React.PureComponent {
       textValue,
     } = this.state;
 
+    if (textValue.length < 2 || textValue.charCodeAt() === 10 || textValue.charCodeAt() === 32) return;
+
     createMessage({
       data: {
         text: textValue,
@@ -32,18 +37,39 @@ export default class MessageForm extends React.PureComponent {
         chatId,
       },
     });
+
+    this.setState({ textValue: ''})
+  }
+
+  onKeyUp = (event) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      this.createMessage();
+      event.target.value = '';
+    }
+  }
+
+  onKeyDown = (e) => {
+    const textValue = this.state.textValue;
+    if (textValue.charCodeAt() === 10 || textValue.charCodeAt() === 32 || isNaN(textValue.charCodeAt())) {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        e.preventDefault()
+      }
+    } 
   }
 
   render() {
     return (
       <Paper className={styles.messageForm}>
         <TextField
+          ref={this.textField}
           className={styles.text}
           placeholder="Введите текст"
           multiline
           label="Текст"
           id="form-text"
           onChange={this.onChange('textValue')}
+          onKeyUp={this.onKeyUp}
+          onKeyDown={this.onKeyDown}
           variant="standard"
         />
         <Fab
