e1ebae8b618f26773f49589d330bdbb657ab0f4d
suika1 <vanyoktank1@gmail.com>
2019-07-21 17:59:05 +0300

    export db from mongo-connection setup

--- backend/src/setup/mongo-connection.js
+++ backend/src/setup/mongo-connection.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
 config();
+export let db = {};
 
 (() => {
   const dev_db_url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASS}@cluster0-jgkmz.mongodb.net/cake_chat_db?retryWrites=true`;
   const mongoDB = process.env.MONGODB_URI || dev_db_url;
   mongoose.connect(mongoDB, {useNewUrlParser: true});
-  const db = mongoose.connection;
+  db = mongoose.connection;
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 })();
