81ca537cb1ccf9e3e0a38ad74f90d4cf2a71c0a0
suika1 <vanyoktank1@gmail.com>
2019-07-21 17:59:49 +0300

    rework getAllChats: return only chats for current user, join users on messages.authorId

--- backend/src/controllers/chat.js
+++ backend/src/controllers/chat.js
@@ -1,3 +1,4 @@
+import lodash from 'lodash';
 import { ChatModel as Chat } from '../models/chat';
 import { UserModel as User } from '../models/user';
 
@@ -44,16 +45,64 @@ export const chatCreate = async (req, res, next) => {
   }
 };
 
-// TODO: return only chats in which current user participate
 export const getAllChats = async (req, res, next) => {
-  Chat.find((err, result) => {
-    if (err) return next(err);
+  try {
+    const {
+      email
+    } = req.decoded;
+
+    const user = await User.findOne({
+      email,
+    });
+    const userChatList = user.chatList.map(a => JSON.stringify(a));
+
+    const chats = await Chat.aggregate([{
+        $unwind: {
+          path: '$messages',
+          preserveNullAndEmptyArrays: true,
+        },
+      }, {
+        $lookup: {
+          from: 'users',
+          localField: 'messages.authorId',
+          foreignField: '_id',
+          as: 'userObjects',
+        },
+      }, {
+        $unwind: {
+          path: '$userObjects',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $group: {
+          _id: '$_id',
+          name: { $first: '$name' },
+          authorId: { $first: '$authorId' },
+          userList: { $push: '$userList' },
+          messages: { $push: '$messages' },
+          userObjects: { $push: '$userObjects' }
+        }
+      }
+    ]);
+    const chatsForCurrentUser = chats.filter(chat => userChatList.includes(JSON.stringify(chat._id)));
+
+    chatsForCurrentUser.forEach(chat => {
+      chat.messages.forEach(message => {
+        const author = chat.userObjects.find(user => JSON.stringify(user._id) === JSON.stringify(message.authorId));
+        message.author = lodash.pick(author, ['_id', 'name', 'email']);
+        delete message.authorId;
+      });
+      delete chat.userObjects;
+    });
 
     return utils.generateResponse({
       res,
-      results: result,
+      results: chatsForCurrentUser,
     });
-  });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 export const getChatById = async (req, res, next) => {
