d19f01a331cd5955601f145db08acd3bebf71b8e
suika1 <vanyoktank1@gmail.com>
2019-07-23 21:56:36 +0300

    fix tr util function, add getProp util function

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,16 +1,29 @@
-export function getDocHeight() {
-  return Math.max(
-    document.body.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.clientHeight,
-    document.documentElement.scrollHeight,
-    document.documentElement.offsetHeight,
-  );
-}
+export const getDocHeight = () => Math.max(
+  document.body.scrollHeight,
+  document.body.offsetHeight,
+  document.documentElement.clientHeight,
+  document.documentElement.scrollHeight,
+  document.documentElement.offsetHeight,
+);
 
 export const tr = (str, length) => {
   if (!str) return;
   if (str.length < length) return str;
 
-  return str.slice(0, length) + '...';
+  return `${str.slice(0, length)}...`;
+}
+
+/**
+ * Helps to avoid TypeError
+ * Example of usage: getProp(() => a.b.c.d.e.f, 'defaultStr')
+ */
+export const getProp = (lambda, defaultValue) => {
+  try {
+    return lambda();
+  } catch (err) {
+    if (err instanceof TypeError) {
+      return defaultValue;
+    }
+    throw err;
+  }
 }
