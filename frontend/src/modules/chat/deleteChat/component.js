73260fcbc56e1e3191ad9dc9af5a07c16b70176d
suika1 <vanyoktank1@gmail.com>
2019-12-09 19:45:28 +0300

    add confirm delete chat modal

--- frontend/src/modules/chat/deleteChat/component.js
+++ frontend/src/modules/chat/deleteChat/component.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+} from '@material-ui/core';
 
-export default ({ deleteChat, chatId }) => (
-  <Button onClick={() => deleteChat({ chatId })}>
-    Delete
-  </Button>
-)
+import s from './styles.scss';
+
+export default ({ deleteChat, chatId }) => {
+  const [isConfirmDeleteModalOpen, setIsConfirmDeleteModalOpen] = useState(false);
+
+  const closeModal = () => setIsConfirmDeleteModalOpen(false);
+  const openModal = () => setIsConfirmDeleteModalOpen(true);
+
+  return (
+    <>
+      <Dialog className={s.deleteChatModal} open={isConfirmDeleteModalOpen} onClose={closeModal}>
+        <DialogTitle>Confirm chat deletion</DialogTitle>
+        <DialogActions>
+          <Button className={s.cancelBtn} onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button
+            className={s.deleteBtn}
+            onClick={() => {
+              deleteChat({ chatId });
+              closeModal();
+            }}
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Button onClick={openModal}>
+        Delete
+      </Button>
+    </>
+  )
+}
