5c8f23d1d1541d3063054dc50d2b7bd7d02db51f
suika1 <vanyoktank1@gmail.com>
2020-01-11 00:59:24 +0300

    fix lint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './modules/app';
-
-import history from './api/browserHistory';
 
 import 'styles/index.css';
 
