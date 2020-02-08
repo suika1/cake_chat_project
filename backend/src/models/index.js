a21691acae19e638758d56ecf9286ea8cdf5a8e8
suika1 <vanyoktank1@gmail.com>
2020-02-08 17:29:45 +0300

    minor module exports changes

---
+++ backend/src/models/index.js
@@ -0,0 +1,9 @@
+import { ChatModel as Chat } from './chat';
+import { UserModel as User } from './user';
+import { MessageModel as Message } from './message';
+
+export default {
+  Chat,
+  User,
+  Message,
+};
