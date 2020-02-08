55482f6b595d5272f5a559b65ef9f0d64e29c9c2
suika1 <vanyoktank1@gmail.com>
2020-02-08 17:30:49 +0300

    implement new chatsUsers api

--- backend/src/config/urls.js
+++ backend/src/config/urls.js
@@ -3,9 +3,11 @@ const api = '/api/v1/';
 export const chatList = `${api}chat-list/`;
 export const messageList = `${api}message-list/`;
 export const users = `${api}users/`;
+export const chatsUsers = `${api}chats-users/`;
 
 export default {
   chatList,
   messageList,
   users,
+  chatsUsers,
 }
